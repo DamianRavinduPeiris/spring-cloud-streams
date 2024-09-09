92f6d91405649b17e41bb7a79a088cc8d89600de
DamianRavinduPeiris <drpeiris3@gmail.com>
2024-09-09 12:12:57 +0530

    Initial Commit.

---
+++ src/main/java/com/damian/scs/ScsPublisherApplication.java
@@ -0,0 +1,13 @@
+package com.damian.scs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScsPublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScsPublisherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/damian/scs/ScsPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damian.scs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScsPublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
